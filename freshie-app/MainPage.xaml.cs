f75b5761b91ecaa34b7ff2734cf03790b31e74eb
xpatxj <patrycjagrzesiak100@gmail.com>
2023-10-26 21:26:23 +0200

    changed login to email and added sign in/sign up

--- freshie-app/MainPage.xaml.cs
+++ freshie-app/MainPage.xaml.cs
@@ -11,10 +11,17 @@ namespace freshie_app
 
         private void OnLoginButtonClicked(object sender, EventArgs e)
         {
-            string login = LoginEntry.Text;
+            string login = EmailEntry.Text;
             string password = PasswordEntry.Text;
 
             
+        }
+        private void OnRegisterTapped(object sender, EventArgs e)
+        {
+            // Obsługa kliknięcia - przenieś użytkownika do strony rejestracji
+            // Możesz tutaj użyć nawigacji do przekierowania użytkownika.
+            // Przykład:
+            // Navigation.PushAsync(new RegistrationPage());
         }
     }
 }
