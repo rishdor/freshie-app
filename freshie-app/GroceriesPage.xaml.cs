9fb957fe6b27d6fbffd787a812f9107098e007c1
xpatxj <patrycjagrzesiak100@gmail.com>
2023-11-01 18:23:43 +0100

    added groceries and recipies pages

---
+++ freshie-app/GroceriesPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace freshie_app;
+
+public partial class GroceriesPage : ContentPage
+{
+	public GroceriesPage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ freshie-app/RecipiesPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace freshie_app;
+
+public partial class RecipiesPage : ContentPage
+{
+	public RecipiesPage()
+	{
+		InitializeComponent();
+	}
+}
