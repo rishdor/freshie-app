cf74b70679b4121be67e0fdc22b704e254abea90
rina <doroshukmaryna@gmail.com>
2023-11-12 23:14:15 +0100

    foolproofed login and registration

--- freshie-app/DTO/ApiClient.cs
+++ freshie-app/DTO/ApiClient.cs
@@ -6,6 +6,7 @@ using System.Net.Http.Json;
 using System.Text;
 using System.Threading.Tasks;
 using System.Net.Http;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 
 namespace freshie_app.DTO
 {
@@ -18,12 +19,21 @@ namespace freshie_app.DTO
             _client = new HttpClient { BaseAddress = new Uri("https://freshie-fridgehub-api.azurewebsites.net/") };
         }
 
-        public static async Task<bool> RegisterUser(User user)
+        public static async Task<string> RegisterUser(User user)
         {
             HttpResponseMessage response = await _client.PostAsJsonAsync("api/Users/register", user);
-            response.EnsureSuccessStatusCode();
-            return response.IsSuccessStatusCode;
+
+            if (response.IsSuccessStatusCode)
+            {
+                return "Registration successful.";
+            }
+            else
+            {
+                string message = await response.Content.ReadAsStringAsync();
+                return message;
+            }
         }
+
         public static async Task<User> LoginUser(string email, string password)
         {
             User user = null;
@@ -35,5 +45,6 @@ namespace freshie_app.DTO
             }
             return user;
         }
+
     }
 }
--- freshie-app/RegisterPage.xaml.cs
+++ freshie-app/RegisterPage.xaml.cs
@@ -14,74 +14,62 @@ public partial class RegisterPage : ContentPage
         string pattern = @"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$";
         return Regex.IsMatch(email, pattern);
     }
-    //figure out how to get this error though api
-    //public bool CheckEmail(string email)
-    //{
-    //    var user = _context.Users.FirstOrDefault(u => u.Email == email);
-    //    return user != null;
-    //}
     public RegisterPage()
 	{
 		InitializeComponent();
 	}
     private async void SignUpButtonClicked(object sender, EventArgs e)
     {
+        EmailCheckDB.IsVisible = false;
+        EmptyField.IsVisible = false;
+        UnmatchingPasswords.IsVisible = false;
+        EmailWrongFormat.IsVisible = false;
+        PasswordCheck.IsVisible = false;
+        TermsOfUse.IsVisible = false;
+
         if (string.IsNullOrWhiteSpace(Password.Text) || string.IsNullOrWhiteSpace(Email.Text) || string.IsNullOrWhiteSpace(Name.Text) || string.IsNullOrWhiteSpace(Password2.Text))
         {
             EmptyField.IsVisible = true;
             return;
         }
-        EmptyField.IsVisible = false;
-
         if (Password.Text != Password2.Text)
         {
             UnmatchingPasswords.IsVisible = true;
             return;
         }
-        UnmatchingPasswords.IsVisible = false;
-
         if (!ValidateEmailPattern(Email.Text))
         {
             EmailWrongFormat.IsVisible = true;
             return;
         }
-        EmailWrongFormat.IsVisible = false;
-
-        //if (CheckEmail(Email.Text))
-        //{
-        //    EmailCheckDB.IsVisible = true;
-        //    return;
-        //}
-        //EmailCheckDB.IsVisible = false;
-
         if (!CheckPasswordCriteria(Password.Text))
         {
             PasswordCheck.IsVisible = true;
             return;
         }
-        PasswordCheck.IsVisible = false;
-
         if (!PrivacyPolicy.IsChecked)
         {
             TermsOfUse.IsVisible = true;
             return;
         }
-        TermsOfUse.IsVisible = false;
-
         string email = Email.Text;
         string password = Password.Text;
         string name = Name.Text;
 
         var user = new User { Email = email, Password = password, Name = name };
-        bool registration = await ApiClient.RegisterUser(user);
-        if (registration)
+        string registration = await ApiClient.RegisterUser(user);
+        if (registration == "A user with this email already exists.")
         {
-            //await DisplayAlert("successful registration", "yay", "ok");
+            EmailCheckDB.IsVisible = true;
+            return;
+        }
+        if (registration == "Registration successful.")
+        {
             await Navigation.PushAsync(new HomePage(user));
         }
         else
         {
-            await DisplayAlert("nope", "something went wrong", "ok");
+            await DisplayAlert("Registration was unsuccessful", "Try again", "OK");
         }
     }
 
