179d4e679134cb3ba0c33e0124d55ee74251086b
rina <doroshukmaryna@gmail.com>
2023-11-12 21:10:45 +0100

    added dto and apiclient methods to login and register

---
+++ freshie-app/DTO/ApiClient.cs
@@ -0,0 +1,39 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+
+namespace freshie_app.DTO
+{
+    public class ApiClient
+    {
+        private static readonly HttpClient _client;
+
+        static ApiClient()
+        {
+            _client = new HttpClient { BaseAddress = new Uri("https://freshie-fridgehub-api.azurewebsites.net/") };
+        }
+
+        public async Task<bool> RegisterUser(User user)
+        {
+            HttpResponseMessage response = await _client.PostAsJsonAsync("api/Users/register", user);
+            response.EnsureSuccessStatusCode();
+            return response.IsSuccessStatusCode;
+        }
+        public static async Task<User> LoginUser(string email, string password)
+        {
+            User user = null;
+            HttpResponseMessage response = await _client.GetAsync($"api/users/login?email={email}&password={password}");
+
+            if (response.IsSuccessStatusCode)
+            {
+                user = await response.Content.ReadAsAsync<User>();
+            }
+            return user;
+        }
+    }
+}
---
+++ freshie-app/DTO/GroceriesHistory.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace freshie_app.DTO
+{
+    public class GroceriesHistory
+    {
+        public int? ProductId { get; set; }
+
+        public int? UserId { get; set; }
+
+        public DateTime? Date { get; set; }
+
+        public bool? Purchased { get; set; }
+
+        public int Id { get; set; }
+
+        public virtual Product? Product { get; set; }
+
+        public virtual User? User { get; set; }
+    }
+}
