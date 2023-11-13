4f6f0b9638eab6d79c21376cdb6e8ff3dbaedba2
rina <doroshukmaryna@gmail.com>
2023-11-13 17:45:40 +0100

    added proper models

---
+++ freshie-webAPI/Models/Diet.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace freshie_webAPI.Models;
+
+public partial class Diet
+{
+    public int DietId { get; set; }
+
+    public string DietName { get; set; } = null!;
+
+    public virtual ICollection<ProductDiet> ProductDiets { get; set; } = new List<ProductDiet>();
+}
---
+++ freshie-webAPI/Models/GroceriesHistory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace freshie_webAPI.Models;
+
+public partial class GroceriesHistory
+{
+    public int Id { get; set; }
+
+    public int? ProductId { get; set; }
+
+    public int? UserId { get; set; }
+
+    public DateTime? Date { get; set; }
+
+    public bool? Purchased { get; set; }
+
+    public virtual Product? Product { get; set; }
+
+    public virtual User? User { get; set; }
+}
