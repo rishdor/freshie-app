b2bcabd7bd7778016a4b177e7408c2768113a18f
emili <kostrubala.em@gmail.com>
2023-11-26 21:14:05 +0100

    Added recipes with related tables to Model and Controllers

---
+++ freshie-webAPI/Models/Course.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace freshie_webAPI.Models
+{
+    [Table("Course")]
+        public class Course
+        {
+            [Key]
+            [Column("course_id")]
+            public int CourseId { get; set; }
+            [Column("course_name")]
+            public string CourseName { get; set; } = null!;
+        }
+ 
+}
