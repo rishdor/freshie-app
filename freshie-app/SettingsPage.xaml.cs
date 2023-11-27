edb74dc20438d93e85389cfb94d3dac635f152cd
xpatxj <patrycjagrzesiak100@gmail.com>
2023-11-27 22:29:33 +0100

    changed settingspage

--- freshie-app/SettingsPage.xaml.cs
+++ freshie-app/SettingsPage.xaml.cs
@@ -1,9 +1,15 @@
+using freshie_app.DTO;
+
 namespace freshie_app;
 
 public partial class SettingsPage : ContentPage
 {
-	public SettingsPage()
+	private User _user;
+	public SettingsPage(User user)
 	{
+		_user = user;
 		InitializeComponent();
+		BindingContext = user;
+		WelcomeLabel.Text = $"Hello {_user.Name}!\nThis is your settings page.";
 	}
 }
