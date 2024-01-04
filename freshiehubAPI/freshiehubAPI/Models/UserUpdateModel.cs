d6555f6a83e5c12d5251f63ca21b119ab8b378fc
rina <doroshukmaryna@gmail.com>
2024-01-04 19:57:56 +0100

    added userupdatemodel

--- freshiehubAPI/freshiehubAPI/Models/UserUpdateModel.cs
+++ freshiehubAPI/freshiehubAPI/Models/UserUpdateModel.cs
@@ -2,5 +2,10 @@
 {
     public class UserUpdateModel
     {
+        public string OldPassword { get; set; }
+        public string NewPassword { get; set; }
+        public string Email { get; set; }
+        public string Name { get; set; }
     }
+
 }
